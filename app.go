74f3bd6ea6bad6c641c9bb6c98673929b53e7e35
JornLee <sbpfikdm@gmail.com>
2026-08-19 07:53:20 +0800

    .

--- app.go
+++ app.go
@@ -98,7 +98,7 @@ func (a *App) Shutdown(ctx context.Context) {
 
 // GetVersion returns the application version.
 func (a *App) GetVersion() string {
-	return "v0.8.47"
+	return "v0.8.48"
 }
 
 // GetDataDir returns the configured binary data directory.
